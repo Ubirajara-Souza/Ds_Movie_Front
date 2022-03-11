4b75bd98c48da91f9a838acc85a93b32ea4acdf2
Ubirajara Souza <bira.souza2015@hotmail.com>
2022-03-11 10:31:25 -0300

    useState, useEffect

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -2,14 +2,21 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import {BASE_URL} from "utils/requests";
+import {useEffect, useState} from "react";
+import {MoviePage} from "types/movie";
 
 function Listing() {
 
-    // forma errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setpageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setpageNumber(data.number);
+            });
+    }, []);
+
 
     return (
         <>
