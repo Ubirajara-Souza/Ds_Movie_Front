04e8cce45b5c7611e0e48a8b2379b4b18dc7e61f
Ubirajara Souza <bira.souza2015@hotmail.com>
2022-03-11 12:09:45 -0300

    Pagination

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -30,9 +30,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber: number) => {
+        setpageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination/>
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
